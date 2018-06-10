f21d550612efdafe1532a0330f552265198e2efd
Clement Thuault <thuaultc@gmail.com>
2018-06-10 16:50:51 +0200

    Fix packaging issues

--- api/pkg/mongo/mongo.go
+++ api/pkg/mongo/mongo.go
@@ -1,7 +1,6 @@
 package mongo
 
 import (
-	"errors"
 	"log"
 
 	"github.com/globalsign/mgo"
@@ -37,20 +36,10 @@ func (c *Conn) Connect(url string) {
 	return &e, nil
 }*/
 
-func (m *MongoDB) Read(id string) (event *models.Event, err error) {
+// Read finds the event with the specific id
+func (c *Conn) Read(id string) (event *models.Event, err error) {
 	err = db.C("events").Find(bson.M{"_id": id}).One(&event)
 	return event, err
-}
-
-func Read(id string) (*event.Event, error) {
-	e, ok := events[id]
-	if !ok {
-		return nil, errors.New("not found")
-	}
-
-	e.ID = id
-
-	return &e, nil
 }
 
 /*func Update(id string) (*Event, error) {
