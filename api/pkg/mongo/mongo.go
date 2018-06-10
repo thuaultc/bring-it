931b71df92343063a49bff52a98fe98c1b3c5c49
Clement Thuault <thuaultc@gmail.com>
2018-06-10 16:46:27 +0200

    Add mongo connection and read functions

---
+++ api/pkg/mongo/mongo.go
@@ -0,0 +1,65 @@
+package mongo
+
+import (
+	"errors"
+	"log"
+
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+	"github.com/thuaultc/bring-it/api/pkg/models"
+)
+
+// Conn structure contains the connection configuration to the database
+type Conn struct {
+	Server   string
+	Database string
+}
+
+var db *mgo.Database
+
+// Connect establishes the connection to the database
+func (c *Conn) Connect(url string) {
+	session, err := mgo.Dial(c.Server)
+	if err != nil {
+		log.Fatal(err)
+	}
+	db = session.DB(c.Database)
+}
+
+/*func Create(id string) (*Event, error) {
+	e, ok := events[id]
+	if !ok {
+		return nil, errors.New("not found")
+	}
+
+	e.ID = id
+
+	return &e, nil
+}*/
+
+func (m *MongoDB) Read(id string) (event *models.Event, err error) {
+	err = db.C("events").Find(bson.M{"_id": id}).One(&event)
+	return event, err
+}
+
+func Read(id string) (*event.Event, error) {
+	e, ok := events[id]
+	if !ok {
+		return nil, errors.New("not found")
+	}
+
+	e.ID = id
+
+	return &e, nil
+}
+
+/*func Update(id string) (*Event, error) {
+	e, ok := events[id]
+	if !ok {
+		return nil, errors.New("not found")
+	}
+
+	e.ID = id
+
+	return &e, nil
+}*/
