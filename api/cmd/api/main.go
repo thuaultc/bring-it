724a47f73fb80449ffa4accc1086404595be7b99
Clement Thuault <thuaultc@gmail.com>
2018-11-04 00:59:22 +0100

    Add specific origin handling for CORS

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -97,11 +97,17 @@ func APIEventRead(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleCORS(w http.ResponseWriter, r *http.Request) bool {
-	if origin := r.Header.Get("Origin"); origin != "" {
-		w.Header().Set("Access-Control-Allow-Origin", origin)
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	authorizedOrigin := os.Getenv("CORS_ALLOWED_ORIGIN")
+
+	if authorizedOrigin == "" {
+		if origin := r.Header.Get("Origin"); origin != "" {
+			authorizedOrigin = origin
+		}
 	}
+
+	w.Header().Set("Access-Control-Allow-Origin", authorizedOrigin)
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
 	// Stop here if its Preflighted OPTIONS request
 	if r.Method == "OPTIONS" {
