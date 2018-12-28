ee57f7156e0fd62776b5acccac2e304ee36f3e65
Clement Thuault <thuaultc@gmail.com>
2018-12-28 18:44:27 +0100

    Add material ui to webapp

--- web/src/Components/App.js
+++ web/src/Components/App.js
@@ -6,33 +6,37 @@ import Storage from "../Storage";
 import Home from "./Routes/Home";
 import Event from "./Routes/EventFromStorage";
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 export const store = new Storage();
 
 class Header extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="container">
-            <a className="navbar-brand" href="/">
-              Bring it!
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-              onClick={this.handleCreate}
-            >
-              <span className="navbar-toggler-icon" />
-            </button>
-          </div>
-        </nav>
-        <br />
-      </React.Fragment>
+      <MuiThemeProvider>
+        <React.Fragment>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+              <a className="navbar-brand" href="/">
+                Bring it!
+              </a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+                onClick={this.handleCreate}
+              >
+                <span className="navbar-toggler-icon" />
+              </button>
+            </div>
+          </nav>
+          <br />
+        </React.Fragment>
+      </MuiThemeProvider>
     );
   }
 }
